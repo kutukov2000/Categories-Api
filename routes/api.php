b2ee312580408da5d04f7fd6b976c37c5583ec87
kutukov2000 <kutukov16.2005@gmail.com>
2023-12-17 17:50:00 +0200

    feat:add create function

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get("categories",[CategoryController::class, 'getAll']);
+Route::post("categories",[CategoryController::class, 'create']);
