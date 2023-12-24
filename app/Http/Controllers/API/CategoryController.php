de47d5a2907b03c98f58ceba19275d3f6e20e9a2
kutukov2000 <kutukov16.2005@gmail.com>
2023-12-24 14:39:24 +0200

    feat: configure Swagger ✨

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -10,12 +10,42 @@ use Intervention\Image\Drivers\Gd\Driver;
 
 class CategoryController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     tags={"Category"},
+     *     path="/api/categories",
+     *     @OA\Response(response="200", description="List Categories.")
+     * )
+     */
     function getAll()
     {
         $list = Categories::all();
         return response()->json($list, 200, ['Charset' => 'utf-8']);
     }
 
+    /**
+     * @OA\Post(
+     *     tags={"Category"},
+     *     path="/api/categories",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"name","image"},
+     *                 @OA\Property(
+     *                     property="image",
+     *                     type="file",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Add Category.")
+     * )
+     */
     function create(Request $request)
     {
         $input = $request->all();
@@ -35,16 +65,34 @@ class CategoryController extends Controller
         foreach ($sizes as $size) {
             $imageSave = $manager->read($image);
             $imageSave->scale(width: $size);
-            $imageSave->toWebp()->save($folderPath ."/".$size."_". $imageName);
+            $imageSave->toWebp()->save($folderPath . "/" . $size . "_" . $imageName);
         }
 
-        $input["image"]=$imageName;
+        $input["image"] = $imageName;
         $category = Categories::create($input);
 
         return response()->json($category, 200, ["Charset" => "utf-8"]);
     }
 
-    function delete($id){
+    /**
+     * @OA\Delete(
+     *     tags={"Category"},
+     *     path="/api/categories/{id}",
+     *     @OA\Response(response="200", description="Delete Category."),
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the category to delete",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     )
+     * )
+     */
+    function delete($id)
+    {
         Categories::destroy($id);
 
         return response()->json('Successfully deleted!', 200, ["Charset" => "utf-8"]);
